6f24f528454eaad370188294c6e79e00769c89da
Dohs <spinery@hotmail.fr>
2019-04-16 17:54:03 +0200

    create nav tab stage

--- src/Controller/StageController.php
+++ src/Controller/StageController.php
@@ -19,7 +19,6 @@ class StageController extends AbstractController
     {
         $stageCamp = new StageManager();
         $stages = $stageCamp->selectAll();
-        var_dump($stages);
         return $this->twig->render('Stage/index.html.twig', ['stages' => $stages]);
     }
 }
