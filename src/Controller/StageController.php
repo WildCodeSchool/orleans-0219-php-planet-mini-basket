c576f296e4140f805e1751f905da11297f782d38
Dohs <spinery@hotmail.fr>
2019-04-16 16:25:26 +0200

    Create Stage controller

---
+++ src/Controller/StageController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Model\StageManager;
+
+class StageController extends AbstractController
+{
+    /**
+     * Display item listing
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        $stageCamp = new StageManager();
+        $stages = $stageCamp->selectAll();
+
+        return $this->twig->render('Stage/index.html.twig', ['stages' => $stages]);
+    }
+}
