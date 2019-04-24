12aecf924077b0b61db4a1fca095ec8a78e93bd7
Jaffar Mallem <jmtrading@live.com>
2019-04-24 13:07:20 +0200

    created files view and controller for admin side

---
+++ src/Controller/AdminFaqController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Model\FaqManager;
+
+class FaqController extends AbstractController
+{
+    /**
+     * Display home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        $faqManager = new FaqManager();
+        $faqs = $faqManager->selectAll();
+        return $this->twig->render('Faq/adminFaq.html.twig', [
+            'faqs' => $faqs
+        ]);
+    }
+}
