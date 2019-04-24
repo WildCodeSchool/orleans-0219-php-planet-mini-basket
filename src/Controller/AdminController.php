1253680045f7ad2e2d4c985a17072e7ebc431227
Dohs <spinery@hotmail.fr>
2019-04-24 19:32:07 +0200

    Create Admin news add query

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,7 +3,14 @@
 
 namespace App\Controller;
 
-class AdminControler extends AbstractController
-{
+use App\Model\NewsManager;
 
+class AdminController extends AbstractController
+{
+    public function news()
+    {
+        $newsManager = new NewsManager();
+        $news = $newsManager->selectAll();
+        return $this->twig->render('Admin/news/news.html.twig', ['news' => $news]);
+    }
 }
