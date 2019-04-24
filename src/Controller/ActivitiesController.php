2b0c7f5a5fe98ba3f82837cfc038705479532fb2
Elodie Billon <elodie.billon1995@outlook.fr>
2019-04-24 11:08:00 +0200

    Création des fichiers pour la vue de la page Activités

---
+++ src/Controller/ActivitiesController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder9
+ * Date: 24/04/19
+ * Time: 11:00
+ */
+
+namespace App\Controller;
+
+use App\Model\ActivitiesManager;
+
+class ActivitiesController extends AbstractController
+{
+    /**
+     * Display activities page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        $activitiesManager = new ActivitiesManager();
+        $activitiesContent = $activitiesManager->selectAll();
+        return $this->twig->render('Activities/index.html.twig', ['news' => $activitiesContent]);
+    }
+}
---
+++ src/Model/ActivitiesManager.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder9
+ * Date: 24/04/19
+ * Time: 11:01
+ */
+
+namespace App\Model;
+
+class ActivitiesManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'news';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+}
