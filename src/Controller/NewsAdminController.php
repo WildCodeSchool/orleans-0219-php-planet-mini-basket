7a69dcb223dddf5de8b561ced5a4e4e407f4dae4
Dohs <spinery@hotmail.fr>
2019-04-26 18:54:10 +0200

    creat admin tools

--- src/Controller/NewsAdminController.php
+++ src/Controller/NewsAdminController.php
@@ -7,10 +7,18 @@ use App\Model\NewsManager;
 
 class NewsAdminController extends AbstractController
 {
-    public function news()
+    public function add()
     {
-        $newsManager = new NewsManager();
-        $news = $newsManager->selectAll();
-        return $this->twig->render('Admin/news/news.html.twig', ['news' => $news]);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $newsManager = new NewsManager();
+            $news = [
+                'news_title' => $_POST['title'],
+                'news_content' => $_POST['content']
+            ];
+            $id = $newsManager->insert($news);
+            header('Location:/Admin/show/' . $id);
+        }
+
+        return $this->twig->render('Admin/add.html.twig');
     }
 }
