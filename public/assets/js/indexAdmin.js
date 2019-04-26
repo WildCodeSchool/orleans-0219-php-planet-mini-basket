221864466f7c6465986426acb709d398cbdb60fb
maiwenn99 <maiwenngay99@gmail.com>
2019-04-26 20:43:23 +0200

    modal for delete

--- public/assets/js/indexAdmin.js
+++ public/assets/js/indexAdmin.js
@@ -1,7 +1,26 @@
-$( document ).ready(function() {
-    $('.trigger').click(function() {
-        $('.modal-wrapper').toggleClass('open');
-        $('.page-wrapper').toggleClass('blur');
-        return false;
+$(function() {
+
+    // Vars
+    var modBtn  = $('#modBtn'),
+        modal   = $('#modal'),
+        close   = modal.find('.close'),
+        modContent = modal.find('.modal-content');
+
+    // open modal when click on open modal button
+    modBtn.on('click', function() {
+        modal.css('display', 'block');
+        modContent.removeClass('modal-animated-out').addClass('modal-animated-in');
     });
+
+    // close modal when click on close button or somewhere out the modal content
+    $(document).on('click', function(e) {
+        var target = $(e.target);
+        if(target.is(modal) || target.is(close)) {
+            modContent.removeClass('modal-animated-in').addClass('modal-animated-out').delay(300).queue(function(next) {
+                modal.css('display', 'none');
+                next();
+            });
+        }
+    });
+
 });
