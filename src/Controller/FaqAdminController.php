f85a824d3d63a3ee35788e7bd04bfe05bceb1025
maiwenn99 <maiwenngay99@gmail.com>
2019-04-30 10:18:22 +0200

    final add for FAQ Admin

--- src/Controller/FaqAdminController.php
+++ src/Controller/FaqAdminController.php
@@ -1,26 +1,46 @@
 <?php
-
-
 namespace App\Controller;
 
-use App\Model\FaqManager;
+use App\Model\FaqAdminManager;
+use App\tools\CleanData;
 
 class FaqAdminController extends AbstractController
 {
-    /**
-     * Display home page
-     *
-     * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
     public function index()
     {
-        $faqManager = new FaqManager();
+        $faqManager = new FaqAdminManager();
         $faqs = $faqManager->selectAll();
-        return $this->twig->render('FaqAdmin/index.html.twig', [
-            'faqs' => $faqs
-        ]);
+        return $this->twig->render('FaqAdmin/index.html.twig', ['faqs' => $faqs]);
+    }
+    public function add()
+    {
+        $error = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $cleanData = new CleanData($_POST);
+            $data = $cleanData->trimData();
+            $faqManager = new FaqAdminManager();
+            var_dump($data);
+            if (empty($data['question_name'])) {
+                $error['question_name'] = 'Veuillez entrer votre question';
+            }
+            if (empty($data['answer_name'])) {
+                $error['answer_name'] = 'Veuillez entre votre réponse';
+            } else {
+                $faq = [
+                    'question_name' => $data['question_name'],
+                    'answer_name' => $data['answer_name']
+                ];
+                var_dump($faq);
+                $id = $faqManager->insert($faq);
+                header('Location:/FaqAdmin/show/' . $id);
+            }
+        }
+        return $this->twig->render('FaqAdmin/add.html.twig', ['error' => $error]);
+    }
+    public function show(int $id)
+    {
+        $faqManager = new FaqAdminManager();
+        $faqs = $faqManager->selectOneById($id);
+        return $this->twig->render('FaqAdmin/show.html.twig', ['faqs' => $faqs]);
     }
 }
---
+++ src/Model/FaqAdminManager.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Model;
+class FaqAdminManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'faq';
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+    public function insert(array $news): int
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("INSERT INTO $this->table (`question_name`,`answer_name`) 
+        VALUES (:question_name,:answer_name)");
+        $statement->bindValue('question_name', $news['question_name'], \PDO::PARAM_STR);
+        $statement->bindValue('answer_name', $news['answer_name'], \PDO::PARAM_STR);
+        if ($statement->execute()) {
+            return (int)$this->pdo->lastInsertId();
+        }
+    }
+}
