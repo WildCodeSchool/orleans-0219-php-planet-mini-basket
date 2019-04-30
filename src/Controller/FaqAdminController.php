1b5f941519d0ab8e05e313721e7be084808d6aa7
maiwenn99 <maiwenngay99@gmail.com>
2019-04-30 11:08:11 +0200

    correction done

--- src/Controller/FaqAdminController.php
+++ src/Controller/FaqAdminController.php
@@ -1,23 +1,23 @@
 <?php
 namespace App\Controller;
-use App\Model\FaqAdminManager;
+use App\Model\FaqManager;
 class FaqAdminController extends AbstractController
 {
     public function index()
     {
-        $faqManager = new FaqAdminManager();
+        $faqManager = new FaqManager();
         $faqs = $faqManager->selectAll();
         return $this->twig->render('FaqAdmin/index.html.twig', ['faqs' => $faqs]);
     }
     public function show(int $id)
     {
-        $faqManager = new FaqAdminManager();
+        $faqManager = new FaqManager();
         $faqs = $faqManager->selectOneById($id);
         return $this->twig->render('FaqAdmin/show.html.twig', ['faqs' => $faqs]);
     }
     public function edit(int $id): string
     {
-        $faqManager = new FaqAdminManager();
+        $faqManager = new FaqManager();
         $faqs = $faqManager->selectOneById($id);
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $faqs['question_name'] = $_POST['question_name'];
--- src/Model/FaqManager.php
+++ src/Model/FaqManager.php
@@ -17,4 +17,18 @@ class FaqManager extends AbstractManager
     {
         parent::__construct(self::TABLE);
     }
+
+    /**
+     * @param array $item
+     * @return bool
+     */
+    public function update(array $faqs):bool
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("UPDATE $this->table SET `question_name` = :question_name, `answer_name` = :answer_name WHERE id=:id");
+        $statement->bindValue('id', $faqs['id'], \PDO::PARAM_INT);
+        $statement->bindValue('question_name', $faqs['question_name'], \PDO::PARAM_STR);
+        $statement->bindValue('answer_name', $faqs['answer_name'], \PDO::PARAM_STR);
+        return $statement->execute();
+    }
 }
