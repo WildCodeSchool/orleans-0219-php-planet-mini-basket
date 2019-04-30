21050f6380458d897a28539f8e739b997bafa722
maiwenn99 <maiwenngay99@gmail.com>
2019-04-30 10:33:44 +0200

    final edit FAQ Admin

--- src/Controller/FaqAdminController.php
+++ src/Controller/FaqAdminController.php
@@ -1,26 +1,29 @@
 <?php
-
-
 namespace App\Controller;
-
-use App\Model\FaqManager;
-
+use App\Model\FaqAdminManager;
 class FaqAdminController extends AbstractController
 {
-    /**
-     * Display home page
-     *
-     * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
     public function index()
     {
-        $faqManager = new FaqManager();
+        $faqManager = new FaqAdminManager();
         $faqs = $faqManager->selectAll();
-        return $this->twig->render('FaqAdmin/index.html.twig', [
-            'faqs' => $faqs
-        ]);
+        return $this->twig->render('FaqAdmin/index.html.twig', ['faqs' => $faqs]);
+    }
+    public function show(int $id)
+    {
+        $faqManager = new FaqAdminManager();
+        $faqs = $faqManager->selectOneById($id);
+        return $this->twig->render('FaqAdmin/show.html.twig', ['faqs' => $faqs]);
+    }
+    public function edit(int $id): string
+    {
+        $faqManager = new FaqAdminManager();
+        $faqs = $faqManager->selectOneById($id);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $faqs['question_name'] = $_POST['question_name'];
+            $faqs['answer_name'] = $_POST['answer_name'];
+            $faqManager->update($faqs);
+        }
+        return $this->twig->render('FaqAdmin/edit.html.twig', ['faqs' => $faqs]);
     }
 }
