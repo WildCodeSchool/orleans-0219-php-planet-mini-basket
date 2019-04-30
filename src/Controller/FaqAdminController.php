22eb555ab30653bac949893effdad3a03e440d4b
maiwenn99 <maiwenngay99@gmail.com>
2019-04-30 11:04:26 +0200

    correction done

--- src/Controller/FaqAdminController.php
+++ src/Controller/FaqAdminController.php
@@ -1,45 +1,43 @@
 <?php
 namespace App\Controller;
 
-use App\Model\FaqAdminManager;
+use App\Model\FaqManager;
 use App\tools\CleanData;
 
 class FaqAdminController extends AbstractController
 {
     public function index()
     {
-        $faqManager = new FaqAdminManager();
+        $faqManager = new FaqManager();
         $faqs = $faqManager->selectAll();
         return $this->twig->render('FaqAdmin/index.html.twig', ['faqs' => $faqs]);
     }
     public function add()
     {
-        $error = [];
+        $errors = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $cleanData = new CleanData($_POST);
             $data = $cleanData->trimData();
-            $faqManager = new FaqAdminManager();
-            var_dump($data);
+            $faqManager = new FaqManager();
             if (empty($data['question_name'])) {
-                $error['question_name'] = 'Veuillez entrer votre question';
+                $errors['question_name'] = 'Veuillez entrer votre question';
             }
             if (empty($data['answer_name'])) {
-                $error['answer_name'] = 'Veuillez entre votre réponse';
+                $errors['answer_name'] = 'Veuillez entre votre réponse';
             } else {
                 $faq = [
                     'question_name' => $data['question_name'],
                     'answer_name' => $data['answer_name']
                 ];
-                var_dump($faq);
                 $id = $faqManager->insert($faq);
                 header('Location:/FaqAdmin/show/' . $id);
             }
         }
-        return $this->twig->render('FaqAdmin/add.html.twig', ['error' => $error]);
+        return $this->twig->render('FaqAdmin/add.html.twig', ['error' => $errors]);
     }
     public function show(int $id)
     {
-        $faqManager = new FaqAdminManager();
+        $faqManager = new FaqManager();
         $faqs = $faqManager->selectOneById($id);
         return $this->twig->render('FaqAdmin/show.html.twig', ['faqs' => $faqs]);
     }
