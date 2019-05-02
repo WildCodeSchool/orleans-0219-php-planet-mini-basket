3463a64b94d09a12f492123eedaa55ca3921089d
Julien Rousseau <julien-rousseau@hotmail.fr>
2019-05-02 10:38:17 +0200

    create controller

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maiwenn
+ * Date: 30/04/2019
+ * Time: 16:28
+ */
+
+namespace App\Controller;
+
+use App\Model\ActivitiesManager;
+use App\tools\CleanData;
+
+
+class AdminController extends AbstractController
+{
+    /**
+     * Display admin page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+
+        return $this->twig->render('Admin/index.html.twig');
+    }
+}
